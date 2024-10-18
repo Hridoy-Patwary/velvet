ede3c13f1a6c68fa728d20f32c941ec5500738a1
Hridoy Patwary <rakibulhasan642004@gmail.com>
2024-10-18 15:28:52 +0600

    inital project setup and changes nothing fancy

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.welcomeMsg}>Welcome to Velvet!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +17,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  welcomeMsg: {
+    color: '#000',
+    fontWeight: 'bold'
+  }
 });
